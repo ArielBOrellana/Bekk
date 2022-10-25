0e086a75d1222dfba3ea23857b3f10b245844105
Ariel Orellana <arielorellana@Ariels-MacBook-Pro.local>
2022-10-25 18:58:47 +0200

    endring av teskst og lenke

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Halla balla :D
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.nrk.no/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Lær deg react i dag... på nrk?
         </a>
       </header>
     </div>
